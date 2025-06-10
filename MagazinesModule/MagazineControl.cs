fbf0ae61d596ac4e11bdb120e2bc32ff74964b9f
josewerlich <werlich@outlook.com>
2025-06-10 18:33:13 -0400

    Updated magazine control

--- MagazinesModule/MagazineControl.cs
+++ MagazinesModule/MagazineControl.cs
@@ -1,37 +1,46 @@
 ﻿
 using Book_Club_2025.ConsoleApp.Shared;
 using Book_Club_2025.ShelfsModule;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Book_Club_2025.MagazinesModule;
 
 public class MagazineControl : BaseEntity
 {
     public int id;
-    public string title;
-    public string editionYear;
-    public string borrowingStatus;
-    public string shelfID;
+    public string Title;
+    public int Edition;
+    public int PublishedYear;
+    public ShelfsControl ShelfsControl;
+    public string Status;
 
-    public MagazineControl(string title, string editionYear, string borrowingStatus, string shelfID)
+    public MagazineControl(string title, int edition, int publishedYear, ShelfsControl shelfsControl)
     {
-        this.title = title;
-        this.editionYear = editionYear;
-        this.borrowingStatus = borrowingStatus;
-        this.shelfID = shelfID;
+        Title = title;
+        Edition = edition;
+        PublishedYear = publishedYear;
+        ShelfsControl = shelfsControl;
+        Status = "Available";
     }
 
     public override string Validate()
     {
         string errors = "";
 
-        if (string.IsNullOrWhiteSpace(title))
+        if (string.IsNullOrWhiteSpace(Title))
             errors += "The field \"Title\" is required.\n";
 
-        else if (title.Length < 3)
+        else if (Title.Length < 3)
             errors += "The field \"Title\" needs at least 3 characters.\n";
 
-        if (editionYear.Length < 3)
-            errors += "The field \"Edition Year\" needs at least 3 characters.\n";
+        if (Edition < 1)
+            errors += "The field \"Edition Year\" needs at least 1 number.\n";
+
+        if (PublishedYear < DateTime.MinValue.Year || PublishedYear > DateTime.Now.Year)
+            errors += "The field \"Published Year\" need to have an past year or this actual year.";
+
+        if (ShelfsControl == null)
+            errors += "The field \"Shelf\" is required.";
 
         return errors;
     }
@@ -39,33 +48,11 @@ public class MagazineControl : BaseEntity
     {
         MagazineControl magazineControl = (MagazineControl)registerUpdated;
 
-        this.title = magazineControl.title;
-        this.editionYear = magazineControl.editionYear;
-        this.borrowingStatus = magazineControl.borrowingStatus;
-        this.shelfID=  magazineControl.shelfID;
-    }
-    public string Borrow()
-    {
-        if (borrowingStatus == "borrowed")
-        {
-            return $"The magazine \"{title}\" is already borrowed.";
-        }
+        Title = magazineControl.Title;
+        Edition = magazineControl.Edition;
+        PublishedYear = magazineControl.PublishedYear;
+        ShelfsControl = magazineControl.ShelfsControl;
 
-        borrowingStatus = "borrowed";
-        return $"The magazine \"{title}\" has been successfully borrowed.";
     }
 
-    public string Return()
-    {
-        if (borrowingStatus == "available")
-        {
-            return $"The magazine \"{title}\" is already available.";
-        }
-
-        borrowingStatus = "available";
-        return $"The magazine \"{title}\" has been successfully returned.";
-    }
 }
-
-
-
