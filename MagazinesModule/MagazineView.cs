79cc41134f68b6c9611e0593c7451f7ba9c23203
josewerlich <werlich@outlook.com>
2025-06-10 18:47:19 -0400

    Updated Magazine View

--- MagazinesModule/MagazineView.cs
+++ MagazinesModule/MagazineView.cs
@@ -1,10 +1,11 @@
 ﻿
 using Book_Club_2025.ConsoleApp.Shared;
+using Book_Club_2025.ShelfsModule;
 
 
 namespace Book_Club_2025.MagazinesModule
 {
-    class MagazineView : BaseView      
+    class MagazineView : BaseView
     {
         private MagazineRepository magazineRepository;
 
@@ -23,8 +24,8 @@ namespace Book_Club_2025.MagazinesModule
             Console.WriteLine();
 
             Console.WriteLine(
-                "{0, -10} | {1, -20} | {2, -30} | {3, -15} | {4, -10 }",
-                "Id", "Title", "Editon Year", "Borrowing Status", "Shelf ID"
+                "{0, -10} | {1, -20} | {2, -30} | {3, -15} | {4, -10 } | {5, -20}",
+                "Id", "Title", "Editon", "Published Year", "Shelf", "Status"
             );
 
             BaseEntity[] magazineControl = magazineRepository.SelectRegister();
@@ -38,7 +39,7 @@ namespace Book_Club_2025.MagazinesModule
 
                 Console.WriteLine(
                    "{0, -10} | {1, -20} | {2, -30} | {3, -16} | {4, -10}",
-                    m.id, m.title, m.editionYear, m.borrowingStatus, m.shelfID
+                    m.id, m.Title, m.Edition, m.PublishedYear, m.ShelfsControl.label, m.Status
                 );
             }
 
@@ -49,19 +50,53 @@ namespace Book_Club_2025.MagazinesModule
             Console.Write("Type the Title: ");
             string title = Console.ReadLine();
 
-            Console.Write("Type the Edition Year: ");
-            string editionYear = Console.ReadLine();
+            Console.Write("Type the Edition: ");
+            int edition = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Type the status -borrowed- or -available-: ");
-            string borrowingStatus = Console.ReadLine();
+            Console.Write("Type the Edition: ");
+            int publishedYear = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Type the ID of the shelf: ");
-            string shelfID = Console.ReadLine();
+            ViewRegister();
 
-            MagazineControl magazineControl = new MagazineControl(title, editionYear, borrowingStatus, shelfID);
 
-            return magazineControl;
+            Console.Write("\nDigite o ID da caixa selecionada: ");
+            int ShelfID = Convert.ToInt32(Console.ReadLine());
+
+            ShelfsControl selectedShelf = (ShelfsControl)magazineRepository.SelectRegisterID(ShelfID);
+
+            MagazineControl magazine = new MagazineControl(title, edition, publishedYear, selectedShelf);
+
+            return magazine;
+        }
+        private void ViewRegister()
+        {
+            Console.WriteLine();
+
+            Console.WriteLine("Shelfs View");
+
+            Console.WriteLine();
+
+            Console.WriteLine(
+                "{0, -10} | {1, -30} | {2, -30} | {3, -30}",
+                "Id", "Label", "Color", "Borrowing Days"
+            );
+
+            BaseEntity[] shelfs = magazineRepository.SelectRegister();
+
+            for (int i = 0; i < shelfs.Length; i++)
+            {
+                ShelfsControl s = (ShelfsControl)shelfs[i];
+
+                if (s == null)
+                    continue;
+
+                Console.WriteLine(
+                  "{0, -10} | {1, -30} | {2, -30} | {3, -30}",
+                    s.id, s.label, s.color, s.borrowingDays
+                );
+            }
         }
     }
+
 }
 
